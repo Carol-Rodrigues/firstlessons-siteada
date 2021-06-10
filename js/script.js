e5dc294ed77d5f16e14cdc9941e10c125b2950d4
CarolArangues <>
2021-06-10 21:13:26 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario)
+
+function validaFormulario () {
+  if (document.getElementById("nome-da-usuaria").value != "" && document.getElementById("email-da-usuaria").value != "" && document.getElementById("tel-da-usuaria").value != "") {
+  alert("Prontinho! Você receberá as novidades por e-mail!!")
+  } else {
+    alert("Por favor, preencha todos os campos!")
+  }
+}
+  
